244dc92ffee088d3d5930db761a2dab64e148ee8
minnypa99 <minnypa99@gmail.com>
2021-07-26 09:52:13 +0900

    Delete turn_startup.sh

--- turn_startup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-EXTIP=$(curl -H "Metadata-Flavor: Google" http://metadata/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip)
-cp -f /etc/turnserver.init /etc/turnserver.conf
-sed -i "s|externalip-here|$EXTIP|" /etc/turnserver.conf
-systemctl restart coturn
