b7f1c3a5b136fb73e40fb5c32487f3a581245e3d
minnypa99 <minnypa99@gmail.com>
2021-08-23 02:08:42 +0900

    Update signurl.js

--- signurl.js
+++ signurl.js
@@ -8,26 +8,28 @@ var base64url = require('base64url');
 const PORT =8000;
 
 const keyName = 'cb-sign-key'
-//const key = Buffer.from(process.env.GCP_SIGNING_KEY, 'base64')
-//const key = 'WvvOqKDciiKZMYws65UdKw=='
-const key = Buffer.from('WvvOqKDciiKZMYws65UdKw==', 'base64')
-const signingURL = 'http://34.149.187.115/'
+const keyVal = 'WvvOqKDciiKZMYws65UdKw=='
 
-const filename = 'car001.png'
-const pathToFile = `rendered/black/${filename}`
-const linkDurationInSeconds = 60
+const keyBytes = Buffer.from(keyVal, 'base64')
 
-// Generate the signed URL
-//const encodedURI = encodeURI(path.join(signingURL, pathToFile))
-const encodedURI = encodeURI('http://34.149.187.115/rendered/black/car001.png');
-const expiration = parseInt(moment().utc().add(linkDurationInSeconds, 'seconds').format('X'))
-const urlToSign = `${encodedURI}?Expires=${expiration}&KeyName=${keyName}`
-const signedURLInBase64 = crypto.createHmac('sha1', key).update(urlToSign).digest('base64')
-const signature = base64url.fromBase64(signedURLInBase64);
-const signedURL = `${urlToSign}&Signature=${signature}`;
-console.log({ signedURL })
+app.get('/signurl', function(req, res) {
 
-app.get('/signcdn', function(req, res) {
+        const signingURL = 'http://34.149.187.115/'
+
+        const filename = 'car001.png'
+        const pathToFile = `rendered/black/${filename}`
+        const linkDurationInSeconds = 600
+
+        // Generate the signed URL
+        const encodedURI = encodeURI(urljoin(signingURL, pathToFile))
+        //const encodedURI = encodeURI('http://34.149.187.115/rendered/black/car001.png');
+        const expiration = parseInt(moment().utc().add(linkDurationInSeconds, 'seconds').format('X'))
+        const urlToSign = `${encodedURI}?Expires=${expiration}&KeyName=${keyName}`
+        const signedURLInBase64 = crypto.createHmac('sha1', keyBytes).update(urlToSign).digest('base64')
+        const signature = base64url.fromBase64(signedURLInBase64);
+        const signedURL = `${urlToSign}&Signature=${signature}`;
+        console.log({ signedURL });
+
         res.send('signedURL is ' + signedURL);
 });
 
